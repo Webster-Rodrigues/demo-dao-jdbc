4b7577ec4d8dfe2c68400e6210ff3eac98d9f3a5
Webster Diogenes <web.driw12@gmail.com>
2023-08-09 12:17:12 -0300

    FindById com Objetos associados

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,12 +1,13 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 	//Operações estáticas p/ intânciar os DAOs
 	
 	public static SellerDao createSellerDao() {
-		return new SellerDaoJDBC();
+		return new SellerDaoJDBC(DB.getConnection());
 		/*A classe expõe um método que retorna o tipo da interface. Mas internamente instância uma implementação
 		 *Ou seja, não precisa expor a implementação direto, pois no programa basta instânciar um SellerDao por meio deste método */
 	}
