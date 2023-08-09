3eac45ea13b787fa092d0df1d8db3eb53e603e1a
Webster Diogenes <web.driw12@gmail.com>
2023-08-09 16:14:45 -0300

    Implementação do Insert

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -25,6 +26,15 @@ public class Program {
 		System.out.println("\n=== TEST 3: seller findAll =====");
 		list = sellerDao.findAll();
 		list.forEach(System.out::println);
+		
+		System.out.println("\n=== TEST 4: seller SellerInsert =====");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.30, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserção concluida! Novo id = " + newSeller.getId());
+		
+		
+		
+		
 	}
 
 }
