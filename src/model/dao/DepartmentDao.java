ca4e397f481c6881f7e242893d1a8e86c997fe93
Webster Diogenes <web.driw12@gmail.com>
2023-08-09 11:00:57 -0300

    Criação das Interfaces Department e Seller

---
+++ src/model/dao/DepartmentDao.java
@@ -0,0 +1,17 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Department;
+
+public interface DepartmentDao {
+	
+	 void insert(Department obj);
+	 void update(Department obj);
+	 void deleteById(Integer id);
+	 Department findById(Integer id); //responsável por consultar no banco se existe obj c/ esse ID 
+	 List<Department> findAll();
+	 
+	 
+	 
+}
