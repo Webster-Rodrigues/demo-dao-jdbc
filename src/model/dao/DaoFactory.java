97ac6e410dfa7ddd48716c732bc1be679ff93233
Webster Diogenes <web.driw12@gmail.com>
2023-08-09 11:13:21 -0300

    SellerDaoJDBC e DaoFactory

---
+++ src/model/dao/DaoFactory.java
@@ -0,0 +1,14 @@
+package model.dao;
+
+import model.dao.impl.SellerDaoJDBC;
+
+public class DaoFactory {
+	//Operações estáticas p/ intânciar os DAOs
+	
+	public static SellerDao createSellerDao() {
+		return new SellerDaoJDBC();
+		/*A classe expõe um método que retorna o tipo da interface. Mas internamente instância uma implementação
+		 *Ou seja, não precisa expor a implementação direto, pois no programa basta instânciar um SellerDao por meio deste método */
+	}
+
+}
